eaa627c2c867ce6883e1016440bcbe30ddb222fb
goatzygames <7hamsterman7@gmail.com>
2024-12-06 22:31:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function DownButtonScroll() {
+    document.getElementById("ProjectsTarget").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+function AboutMeScroll() {
+    document.getElementById("ScrollAboutMe").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
